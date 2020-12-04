b99d574ead685f92f7436d200ae003a38f2b2492
iqbal <iqbal@paket.id>
2020-12-04 09:52:10 +0700

    feat(init): Laravel Instalation
    For initial Commit

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('v1')->group(function () {
+    
+	//Registration
+    Route::name('user/registration')->post('user/registration', 'Auth\RegisterController@create');
+    Route::name('user/registration')->get('user/registration', 'Auth\RegisterController@read');
+    Route::name('user/registration/{id}')->put('user/registration/{id}', 'Auth\RegisterController@update');
+    Route::name('user/registration/{id}')->delete('user/registration/{id}', 'Auth\RegisterController@delete');
+    Route::name('user/registration/{id}')->get('user/registration/{id}', 'Auth\RegisterController@find');
+
+});
